9b6a6f7b03fd39b3966f916635148ee02a2c5677
code-with-zulquar <codewithzulquar@gmail.com>
2024-11-27 23:40:20 +0530

    utils code setup

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = (requestHandler) => {
+    (res, req, next) => {
+         Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+     }
+}
+
+export { asyncHandler };
